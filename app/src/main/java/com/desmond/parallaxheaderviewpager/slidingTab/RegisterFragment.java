a557a0d7aaf8df83b29be5b8942ad930ddec3cda
pengsong <935804382@qq.com>
2016-12-15 14:06:04 +0800

    extends

--- app/src/main/java/com/desmond/parallaxheaderviewpager/slidingTab/RegisterFragment.java
+++ app/src/main/java/com/desmond/parallaxheaderviewpager/slidingTab/RegisterFragment.java
@@ -1,7 +1,15 @@
 package com.desmond.parallaxheaderviewpager.slidingTab;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Administrator on 2016/12/15.
  */
-public class RegisterFragment {
+public class RegisterFragment extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
